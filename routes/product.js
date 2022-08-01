c4995330867926add214532239156753828bb728
Newt-JH <flozzang@naver.com>
2022-08-01 16:30:38 +0900

    setting

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-//const db = require('./../shopping_DB/serUserDB');
+const db = require('./../DB/serProduct');
 const wrap = require('./wrapper');
 const wrapper = wrap.wrapper;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', wrapper(async function(req, res, next) {
+    
+    let f = await db.readProduct();
+    res.send(f);
+    
+}));
 
 module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const db = require('./../DB/serUser');
+const wrap = require('./wrapper');
+const wrapper = wrap.wrapper;
+
+/* 유저 조회 */
+router.get('/', wrapper( async function(req, res, next) {
+  const f = await db.readUser(adsasd);
+  res.send(f);
+}));
+
+
+
+module.exports = router;
