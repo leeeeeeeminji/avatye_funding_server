6365d384e899a28b9d64a2e9723dd4586f16afa2
leeeeeeeminji <ldfu0269@naver.com>
2022-08-04 17:21:33 +0900

    commit

--- DB/serProject.js
+++ DB/serProject.js
@@ -20,7 +20,7 @@ function findProject(req) {
 }
 
 // 프로젝트 등록하기
-function createProject(req) {
+function createProject(req) {   
     const {cateIndex, userIndex, proTitle, proSummary, proProfile, proGoal, proBeginDate, proEndDate, proContents}= req.body;
 
     const query = `insert into project (cateIndex, userIndex, proTitle, proSummary, proProfile, proGoal, proBeginDate, proEndDate, proContents) values (${cateIndex}, ${userIndex}, '${proTitle}', '${proSummary}', '${proProfile}', ${proGoal}, '${proBeginDate}', '${proEndDate}', '${proContents}')`
