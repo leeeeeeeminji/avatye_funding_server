e269e63622898fe7771250ff66397039c1706f39
Newt-JH <flozzang@naver.com>
2022-08-05 17:07:01 +0900

    commit

--- middleware/userMiddleWare.js
+++ middleware/userMiddleWare.js
@@ -1,24 +1,5 @@
 const jwt = require('jsonwebtoken');
 const secret = 'hwan'
-const db = require('../DB/serUserDB');
-
-
-// 토큰 발급
-async function newToken(loginMethod,loginID){
-    const div = await db.readUserDIV(loginMethod,loginID);
-    console.log(div[0].userID);
-    const tokenDiv = div[0].userID;
-
-    token = jwt.sign({
-        type: 'JWT',
-        userDIV: tokenDiv
-    }, secret, {
-        expiresIn: '15m', // 만료시간 15분
-        issuer: tokenDiv
-    });
-
-    return token;
-}
 
 // 토큰 검증
 async function verifyToken(token){
@@ -40,6 +21,5 @@ async function verifyToken(token){
 }
 
 module.exports = {
-    newToken,
     verifyToken
 }
