f0c38fd670e33dfbaca46a6720e0a3104796b1cd
Newt-JH <flozzang@naver.com>
2022-08-11 16:11:18 +0900

    commit

--- routes/project/main.js
+++ routes/project/main.js
@@ -5,26 +5,7 @@ const middle = require('../../middleware/userMiddleWare');
 const wrap = require('../../util/wrapper');
 const wrapper = wrap.wrapper;
 
-const gg = wrapper(async function(req,res,query){
-        // 토큰 가져오기
-        const toke = req.get('user_token');
-        if(toke !== undefined){
-        // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-        const msg = await middle.verifyToken(toke);
-        // 에러 발생 시 res로 에러 반환
-        if (msg.code) {
-            console.log(msg.code + " : " + msg.massage);
-            return res.send({ err: msg.code + " : " + msg.massage });
-        } else {
-            // 유저 DIV 값으로 DB에서 정보 읽어오기
-            console.log(msg.userDIV);
-            query;
-        }
-    }else{
-        console.log("토큰 없음");
-        query;
-    } 
-})
+
 
 // 메인 화면 주목할만한 프로젝트
 router.get('/pointproject', wrapper(async function(req, res) {
@@ -33,7 +14,7 @@ router.get('/pointproject', wrapper(async function(req, res) {
         return res.send(f);
     });
 
-    gg(req,res,query);
+    heartToken(req,res,query);
 
 }));
 
