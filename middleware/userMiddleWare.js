2b9db416221324b830397e3a0fb42255d1ebfc03
Newt-JH <flozzang@naver.com>
2022-08-11 16:53:56 +0900

    commit

--- middleware/userMiddleWare.js
+++ middleware/userMiddleWare.js
@@ -22,28 +22,6 @@ async function verifyToken(token){
     }
 }
 
-const heartToken = wrapper(async function(req,res,query){
-    // 토큰 가져오기
-    const toke = req.get('user_token');
-    if(toke !== undefined){
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
-    } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        query;
-    }
-}else{
-    console.log("토큰 없음");
-    query;
-} 
-})
-
 module.exports = {
     verifyToken,
-    heartToken
 }
--- routes/project/main.js
+++ routes/project/main.js
@@ -6,19 +6,31 @@ const middle = require('../../middleware/userMiddleWare');
 const wrap = require('../../util/wrapper');
 const wrapper = wrap.wrapper;
 
-
-
 // 메인 화면 주목할만한 프로젝트
 router.get('/pointproject', wrapper(async function(req, res) {
-
+    // 토큰 가져오기
+    const toke = req.get('user_token');
+    if(toke !== undefined){
+    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
+    const msg = await middle.verifyToken(toke);
+    // 에러 발생 시 res로 에러 반환
+    if (msg.code) {
+        console.log(msg.code + " : " + msg.massage);
+        return res.send({ err: msg.code + " : " + msg.massage });
+    } else {
+        // 유저 DIV 값으로 DB에서 정보 읽어오기
+        console.log(msg.userDIV);
+        let f = await db.mdProject(msg.userDIV);
+        return res.send(f);
+    }
+}else{
+    console.log("토큰 없음");
     let f = await db.mdProject();
-    let query = wrapper(async function(result){
-        return res.send(result);
-    });
-  
-    await middle.heartToken(req,res,query(f));
+    return res.send(f);
+}
 
 }));
+
 
 // 메인 화면 인기 프로젝트
 router.get('/bestproject', wrapper(async function(req, res) {
